8efa1017cc9888f293470b878628c8c3368afb14
Matthew <matthewajustice@gmail.com>
2023-06-10 01:21:41 -0400

    I hate mobile first design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   important: true,
   theme: {
+    screens: {
+      '2xl': { max: '1535px' },
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' },
+    },
     extend: {
       colors: {
         seasalt: '#F8FAFC',
