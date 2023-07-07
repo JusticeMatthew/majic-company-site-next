3bc19f8397bc2d3d1b8d0df378354a6b8b9f4fa7
Matthew <matthewajustice@gmail.com>
2023-07-07 16:46:39 -0400

    Fixed semantics

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,10 +16,10 @@ const Contact = ({ setInView }) => {
     >
       <div className="w-full bg-seasalt h-[50rem] sg:h-[34rem] rounded-2xl p-24 sg:flex-row flex-col flex max-sg:items-center justify-between">
         <div className="w-60">
-          <h6 className="mb-8 text-5xl font-calistoga">
+          <h5 className="mb-8 text-5xl font-calistoga">
             Request a<br />
             <span className="text-gradient">free quote</span>
-          </h6>
+          </h5>
           <p>
             Get started and request a free quote for your website, video or
             managing your social media platform!
