ee726969c6589241e573c0cef566f4bf0c8f090a
Matthew <matthewajustice@gmail.com>
2023-07-14 19:05:49 -0400

    reverted services section back in preperation for pricing package replacment

--- src/constants/services.js
+++ src/constants/services.js
@@ -2,11 +2,12 @@ const services = [
   { service: 'Custom website design', offset: true },
   { service: 'Search engine optimization', offset: true },
   { service: 'Mobile-friendly design' },
-  { service: 'Google business listing' },
-  { service: 'Personalized domain', offset: true },
-  { service: 'Video integration', offset: true },
-  { service: 'Secure web hosting' },
-  { service: 'Ongoing support' },
+  { service: 'Logo design' },
+  { service: 'Video editing & integration', offset: true },
+  { service: 'Social media integration', offset: true },
+  { service: 'E-commerce platform' },
+  { service: 'Content management system' },
+  { service: 'Secure web hosting', offset: true },
+  { service: 'Ongoing support', offset: true },
 ];
-
 export default services;
